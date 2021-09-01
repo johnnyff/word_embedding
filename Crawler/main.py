0c27ecc3c95909543ef2b86e5edff73ce8d403f0
wotkddl21 <kw0095@naver.com>
2021-09-01 15:01:20 +0900

    first_commit

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,1 +1,138 @@
-/home/wongi/workspace/ELMO/knowledge_based_sentiment_analysis/community_crawler/main.py
+#-*- coding: utf-8 -*-
+
+# Developer : Jeong Wooyoung, EGLAB, Hongik University
+# Contact   : gunyoung20@naver.com
+
+import sys
+import multiprocessing
+from datetime import datetime
+from multi_processor import MultiCrawler
+#include "user_header.h"
+
+
+if __name__ == '__main__' :
+    if sys.platform.startswith('linux'):
+      print("argv[1] :"+sys.argv[1])
+   # if sys.platform.startswith('win'):
+      multiprocessing.freeze_support()
+      multiprocessing.set_start_method("spawn")
+      mc = MultiCrawler()
+#        keywords = ['홈트']
+
+      keywords = sys.argv[1:]
+#        keywords.append(sys.argv[1])
+        
+        #processes = [mc.multiCrawlingDaum]
+        
+        #processes = [mc.multiCrawlingFacebook,mc.multiCrawlingInsta,mc.multiCrawlingHygall, mc.multiCrawlingSlrclub,
+        #       mc.multiCrawlingYgosu, mc.multiCrawlingHumoruniv, mc.multiCrawlingTheqoo, mc.multiCrawlingEtoland,
+        #       mc.multiCrawlingFmkorea, mc.multiCrawlingPann, mc.multiCrawlingBobae, mc.multiCrawlingDcinside,
+        #       mc.multiCrawlingRuliweb, mc.multiCrawlingMlbpark, mc.multiCrawlingInven, mc.multiCrawlingTodayhumor,
+        #       mc.multiCrawlingPpomppu, mc.multiCrawlingClien, mc.multiCrawlingInstiz, mc.multiCrawlingCook82,mc.multiCrawlingNaver]
+        
+#        processes = [mc.multiCrawlingFacebook,mc.multiCrawlingInsta,mc.multiCrawlingHygall, mc.multiCrawlingSlrclub,mc.multiCrawlingNaver,
+#               mc.multiCrawlingYgosu, mc.multiCrawlingHumoruniv, mc.multiCrawlingTheqoo, mc.multiCrawlingEtoland,
+#               mc.multiCrawlingFmkorea, mc.multiCrawlingPann, mc.multiCrawlingBobae, mc.multiCrawlingDcinside,
+#               mc.multiCrawlingRuliweb, mc.multiCrawlingMlbpark, mc.multiCrawlingInven, mc.multiCrawlingTodayhumor,
+#               mc.multiCrawlingPpomppu, mc.multiCrawlingClien, mc.multiCrawlingInstiz, mc.multiCrawlingCook82]
+      if keywords[0] == 'ranknews':
+            processes = [mc.multiCrawlingRankingNews]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+
+      else:
+            # processes = [mc.multiCrawlingInstaDumper]
+            # start = datetime.now()
+            # for keyword in keywords:
+            #       community_start = datetime.now()
+            #       print("Community "+keyword+" crawling start.")
+            #       mc.distributeProcess(keyword, processes)
+            #       sys.stdout.flush()
+            #       print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+            processes = [mc.multiCrawlingNaverblog]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+
+            processes = [mc.multiCrawlingSlrclub]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+      
+            processes = [mc.multiCrawlingtistory]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+
+            processes = [mc.multiCrawlingNaverNews, mc.multiCrawlingDaum, mc.multiCrawlingYgosu, mc.multiCrawlingHygall]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+            processes = [mc.multiCrawlingHumoruniv, mc.multiCrawlingNaver, mc.multiCrawlingTheqoo, mc.multiCrawlingEtoland]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+            
+            processes = [mc.multiCrawlingDcinside,mc.multiCrawlingFmkorea, mc.multiCrawlingPann, mc.multiCrawlingBobae,  mc.multiCrawlingMlbpark]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+                  
+            processes = [mc.multiCrawlingInven, mc.multiCrawlingTodayhumor, mc.multiCrawlingPpomppu, mc.multiCrawlingClien, mc.multiCrawlingInstiz, mc.multiCrawlingCook82, mc.multiCrawlingYoutube ]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+
+
+            processes = [mc.multiCrawlingNavercafe]
+            start = datetime.now()
+            for keyword in keywords:
+                  community_start = datetime.now()
+                  print("Community "+keyword+" crawling start.")
+                  mc.distributeProcess(keyword, processes)
+                  sys.stdout.flush()
+                  print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+
+            # processes = [mc.multiCrawlingInsta]
+            # start = datetime.now()
+            # for keyword in keywords:
+            #       community_start = datetime.now()
+            #       print("Community "+keyword+" crawling start.")
+            #       mc.distributeProcess(keyword, processes)
+            #       sys.stdout.flush()
+            #       print("Community "+keyword+" crawling finish. spend time :",(datetime.now()-community_start))
+            
+      print("Community crawling finish. spend time :",(datetime.now()-start)) 
